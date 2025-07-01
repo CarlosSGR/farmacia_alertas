3cd6ef28f2bb722e63ca6634ed82e1c148bcbe28
Carlos Sebastian Gonzalez Ramirez <grcarlossebastian@gmail.com>
2025-07-01 14:55:03 -0600

    Correccion de errores

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pytest
+import sqlalchemy
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from app import app, db
@@ -8,7 +9,11 @@ from app import app, db
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+    app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+        "connect_args": {"check_same_thread": False},
+        "poolclass": sqlalchemy.pool.StaticPool,
+    }
     with app.app_context():
         db.create_all()
         yield app.test_client()
